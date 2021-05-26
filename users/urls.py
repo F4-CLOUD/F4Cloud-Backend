6b19c78dfad7e3250a047b4d71aeccee3e49dde4
SanghwaLee <devlsh0124@gmail.com>
2021-05-27 03:26:30 +0900

    feat: Update users url

--- users/urls.py
+++ users/urls.py
@@ -4,11 +4,10 @@ from users import views
 urlpatterns = [
     path('sign_up/', views.SignUp.as_view()),
     path('confirm_sign_up/', views.ConfirmSignUp.as_view()),
+    path('sign_in/', views.SignIn.as_view()),
     path('sign_out/', views.SignOut.as_view()),
-    path('admin_initiate_user/', views.AdminInitiateAuth.as_view()),
     path('change_password/', views.ChangePassword.as_view()),
     path('forgot_password/', views.ForgotPassword.as_view()),
     path('confirm_forgot_password/', views.ConfirmForgotPassword().as_view()),
-    #path('retrieve_info/', views.RetrieveInfo.as_view()),
     path('delete_user/', views.DeleteUser.as_view())
 ]
