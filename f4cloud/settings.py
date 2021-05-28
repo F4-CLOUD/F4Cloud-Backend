9eedf10cc22defacd5615ff2566c77b241251766
SanghwaLee <devlsh0124@gmail.com>
2021-05-29 00:18:00 +0900

    feat: Update allow host

--- f4cloud/settings.py
+++ f4cloud/settings.py
@@ -50,7 +50,7 @@ SECRET_KEY = 'django-insecure-0xvkwrbq*-kopr4^$mlyd6(-90hv8ezl=6by28g0glysi(x@47
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
