bf30c9108880d6a6e83d8a5f85a290412a8f035d
SanghwaLee <devlsh0124@gmail.com>
2021-05-28 10:39:54 +0900

    feat: Update file model, serializers

--- files/models.py
+++ files/models.py
@@ -7,12 +7,16 @@ class File(models.Model):
     file_id = models.BigAutoField(auto_created=True,
                                   primary_key=True, serialize=True, verbose_name='ID')
     folder_id = models.ForeignKey(
-        'folders.Folder', related_name='file_location', on_delete=models.CASCADE, db_column='folder_id')
-    s3_url = models.CharField(max_length=255)
+        'folders.Folder', on_delete=models.CASCADE, db_column='folder_id')
+    user_id = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, db_column='user_id')
     name = models.CharField(max_length=255)
+    s3_url = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
-    size = models.IntegerField()
+    size = models.PositiveIntegerField()
+    hash_tag = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         db_table = 'files'
--- files/serializers.py
+++ files/serializers.py
@@ -5,8 +5,10 @@ from .models import File
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = ('file_id', 'folder_id', 's3_url',
-                  'name', 'created_at', 'modified_at', 'size')
+        fields = (
+            'file_id', 'folder_id', 'user_id', 'name', 's3_url', 'path',
+            'created_at', 'modified_at', 'size', 'hash_tag',
+        )
 
 
 class FileNameSerializer(serializers.ModelSerializer):
@@ -18,4 +20,4 @@ class FileNameSerializer(serializers.ModelSerializer):
 class FileMoveSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = ('s3_url', 'folder_id', )
+        fields = ('s3_url', 'folder_id', 'path')
